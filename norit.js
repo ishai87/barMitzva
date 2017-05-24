c2e28d5835724ad03a716dcdb240f13e6d175710
ishai87 <levishai87@gmail.com>
2017-05-24 19:47:36 +0300

    Add files via upload

--- norit.js
+++ norit.js
@@ -3,10 +3,26 @@ var b1= document.getElementById("q");
 b1.onclick=function() {
 
   var x1=document.getElementById("a").value;
-  if (x1=="1948"){
+  if (x1=="שירות") {
     window.location.href="norit1.html";
-    }
-    else {
-    window.alert("טעות.. נסו שוב");
-    }
+  }
+  else if (x1=="שרות"){
+    window.location.href="norit1.html";
+  }
+  else if (x1=="מחלקת שירות"){
+    window.location.href="norit1.html";
+  }
+  else if (x1=="מחלקת שרות"){
+    window.location.href="norit1.html";
+  }
+  else if (x1=="מחלקת השירות"){
+    window.location.href="norit1.html";
+  }
+  else if (x1=="מחלקת השרות"){
+    window.location.href="norit1.html";
+  }
+
+  else {
+  window.alert("טעות.. נסו שוב");
+  }
 }
--- parag.js
+++ parag.js
@@ -3,9 +3,18 @@ var b1= document.getElementById("q");
 b1.onclick=function() {
 
   var x1=document.getElementById("a").value;
-  if (x1=="1948"){
+  if (x1=="אקליפטוס"){
     window.location.href="parag1.html";
-    }
+  }
+  else if (x1=="אקליפתוס"){
+    window.location.href="parag1.html";
+  }
+  else if (x1=="אקלפטוס"){
+    window.location.href="parag1.html";
+  }
+  else if (x1=="אקלפתוס"){
+    window.location.href="parag1.html";
+  }
     else {
     window.alert("טעות.. נסו שוב");
     }
