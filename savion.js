eadcad5beb60c867a27bafad864a3d37a96406fd
ishai87 <levishai87@gmail.com>
2017-05-25 08:57:08 +0300

    Add files via upload

--- savion.js
+++ savion.js
@@ -3,7 +3,7 @@ var b1= document.getElementById("q");
 b1.onclick=function() {
 
   var x1=document.getElementById("a").value;
-  if (x1=="2007"){
+  if (x1=="2005"){
     window.location.href="savion1.html";
     }
     else {
